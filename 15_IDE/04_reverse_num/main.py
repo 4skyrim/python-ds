36dcf7ebb58487418b98c612da142b6160ed07a2
Yaroslava Pravolamskaya <you@example.com>
2022-03-17 01:22:15 +0300

    доделать

--- 15_IDE/04_reverse_num/main.py
+++ 15_IDE/04_reverse_num/main.py
@@ -1,3 +1,21 @@
 # TODO здесь писать код
+N = float(input('enter number '))
+K = float(input('enter number'))
+
+def reverse(N, K):
+  N = str(N)
+  K = str(K)
+  lengthN = len(N)
+  lengthK = len(K)
+  word_1 =  str(N).find('.')
+  word_2 = str(K).find('.')
+  print(reversed(N[int(lengthN) -int(word_1)]))
+
+
+reverse(N, K)
+
+
+
+reverse(N, K)
 
 
