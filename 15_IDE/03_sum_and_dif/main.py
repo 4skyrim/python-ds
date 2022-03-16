5d24a1a21b1e24b87953e2063cdb9acec005431b
Yaroslava Pravolamskaya <you@example.com>
2022-03-17 00:13:51 +0300

    task 3

--- 15_IDE/03_sum_and_dif/main.py
+++ 15_IDE/03_sum_and_dif/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+
+n = int(input('enter number '))
+def summ(n):
+    summ = 0
+    while n != 0:
+        summ += n %10
+        n //= 10
+    print('sum of digits', summ)
+    return(n)
+def count(n):
+    count = 0
+    while n != 0:
+        count += 1
+        n //= 10
+    print('amount of digits ', count)
+    return(n)
+summ(n)
+count(n)
+
+
+
+
