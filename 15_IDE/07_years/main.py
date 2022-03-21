5fa87943f2ea5fda7481eaeac71f3405f228513e
Yaroslava Pravolamskaya <you@example.com>
2022-03-21 23:55:05 +0300

    task 7

--- 15_IDE/07_years/main.py
+++ 15_IDE/07_years/main.py
@@ -1,1 +1,6 @@
-# TODO здесь писать код
+start = int(input('enter number '))
+end = int(input(' enter number '))
+for number in range(start, end + 1):
+    str_number = str(number)
+    for elem in str_number:
+
