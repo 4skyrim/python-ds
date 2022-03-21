c32479c6b8328fb5fb1f7386846f86205112c561
Yaroslava Pravolamskaya <you@example.com>
2022-03-21 22:54:26 +0300

    task 6

--- 15_IDE/06_coin2/main.py
+++ 15_IDE/06_coin2/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+print("Enter coin coordinates:" )
+X = float(input('X = '))
+Y = float(input('Y = '))
+radio = float(input('radio = '))
+
+def radius(X, Y, radio):
+    if X > radio or Y> radio:
+        print('Coin is not here')
+    else:
+        print('U are close to coin')
+
+radius(X, Y, radio)
+
+
