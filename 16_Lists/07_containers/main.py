677cd533c32fc8a6e796e47f9b78e755ddbbe0fe
Yaroslava Pravolamskaya <you@example.com>
2022-04-05 23:22:02 +0300

    доделать 7

--- 16_Lists/07_containers/main.py
+++ 16_Lists/07_containers/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+number = int(input('Enter the number of boxes '))
+for _ in range(number):
+    ask = int(input('Enter the weight of the box '))
+    if ask > 200:
+        print('Error. Number cannot be greater than 200 and cannot be float. Try again. ')
+        ask = int(input('Enter the weight of the box '))
+
+new_box = int(input('enter weight '))
+
+
