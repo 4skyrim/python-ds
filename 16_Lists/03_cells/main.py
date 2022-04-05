4ed7fd556f6dbe01c72de9c3897f1e1e650aa427
Yaroslava Pravolamskaya <you@example.com>
2022-04-05 22:25:07 +0300

    4 task of 16

--- 16_Lists/03_cells/main.py
+++ 16_Lists/03_cells/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+N =int(input('Enter the number of cells '))
+list_n = []
+count = 0
+
+for i in range(1, N):
+    list_n.append(i)
+length = len(list_n)
+for n in range(length):
+    if list_n[n] < count:
+        print(list_n[n])
+    count += 1
+
+
+
+
+
+
+
+
+
--- 16_Lists/04_videocards/main.py
+++ 16_Lists/04_videocards/main.py
@@ -1,3 +1,10 @@
 # TODO здесь писать код
+old_list = [3070, 2060, 3090, 3070, 3090 ]
+new_list = []
+max_number = max(old_list)
+for i in old_list:
+    if i != max_number:
+        new_list.append(i)
+print(new_list, ' - the new list of videocards')
 
 
