1d0a65b22e5bbd95f9908e8d3ee3b59691e9f304
Yaroslava Pravolamskaya <you@example.com>
2022-04-05 22:54:33 +0300

    6 task of 16

--- 16_Lists/06_word_analysis/main.py
+++ 16_Lists/06_word_analysis/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+
+unique_letter = 0
+word = input('enter word ')
+count = 0
+for letter in word:
+    word.count(letter)
+    if  word.count(letter) == 1:
+        unique_letter += 1
+print(unique_letter)
