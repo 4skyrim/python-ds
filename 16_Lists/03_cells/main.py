71463e8e7c9d147d0631aeadf77e1850beb8bd8a
Yaroslava Pravolamskaya <you@example.com>
2022-04-08 23:44:20 +0300

    поправила 2

--- 16_Lists/03_cells/main.py
+++ 16_Lists/03_cells/main.py
@@ -1,14 +1,12 @@
-N =int(input('Enter the number of cells '))
-list_n = []
-count = 0
+cells_list = [1, 2, 6, 8, 5]
+new_list = []
+print('Количество клеток: ', len(cells_list))
+for i in range(len(cells_list)):
+    if  i > cells_list[i]:
+        new_list.append(i)
 
-for i in range(1, N):
-    list_n.append(i)
-length = len(list_n)
-for n in range(length):
-    if list_n[n] < count:
-        print(list_n[n])
-    count += 1
+print(' Эффектиные клетки: ', i)
+
 
 
 
