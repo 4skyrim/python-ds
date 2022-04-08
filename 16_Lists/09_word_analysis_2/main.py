49411457aa6beed43cdd775d0230b40e97a105ac
Yaroslava Pravolamskaya <you@example.com>
2022-04-08 23:34:36 +0300

    9/10

--- 16_Lists/09_word_analysis_2/main.py
+++ 16_Lists/09_word_analysis_2/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+
+word = list(input('Enter word '))
+rev_word = []
+for elem in reversed(word):
+    rev_word.append(elem)
+
+if rev_word == word:
+    print('palindrome ')
+else:
+    print('no palindrome')
