ac3104f546d9df70cf02fa0dfe446e3b43286430
Yaroslava Pravolamskaya <you@example.com>
2022-04-17 00:20:08 +0300

    tasks

--- 17_List_methods/08_rhyme_cnt/main.py
+++ 17_List_methods/08_rhyme_cnt/main.py
@@ -1,1 +1,6 @@
-# TODO здесь писать код
+N = int(input('Введите количество человек: '))
+count_number = int(input('Введите число в считалке '))
+circle = []
+for i in range(N):
+    circle.append(i)
+
