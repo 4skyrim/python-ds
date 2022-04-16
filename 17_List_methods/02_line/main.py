1cc462a6adccea2856bd7699be3d1c471ed86dc2
Yaroslava Pravolamskaya <you@example.com>
2022-04-16 22:20:53 +0300

    tasks

--- 17_List_methods/02_line/main.py
+++ 17_List_methods/02_line/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+list_1 = []
+list_2 = []
+for i_list in range(160, 170, 2):
+    list_1.append(i_list)
+for j_list in range(162,180, 3):
+    list_2.append(j_list)
+list_2.extend(list_1)
+list_2.sort()
+print(' sorted list: ', list_2)
--- 17_List_methods/04_party/main.py
+++ 17_List_methods/04_party/main.py
@@ -1,3 +1,27 @@
-guests = ['Петя', 'Ваня', 'Саша', 'Лиза', 'Катя']
+def party(guests):
 
-# TODO здесь писать код
+ capacity = len(guests)
+ while True:
+     print('Number of guests: ', len(guests), 'Guests: ', guests)
+     ask  =input(' come or leave? ')
+
+     if  capacity < 6:
+         if ask == 'come':
+             capacity += 1
+             name = input('Who is it? ')
+             guests.append(name)
+         elif ask == 'leave':
+            capacity -= 1
+            name = input('Who is it?')
+            guests.remove(name)
+     if capacity > 6:
+        print('Too much. Go away ')
+        print('Number of guests: ', len(guests), 'Guests: ', guests)
+        ask = input(' come or leave? ')
+
+     if ask == 'sleep':
+      break
+
+guests = ['Kate', 'Boris', 'Jake', 'Vah', 'Tori']
+
+party(guests)
