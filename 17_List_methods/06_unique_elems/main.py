240d6c886410ace405dbf6d41ab05008e6ddb91e
Yaroslava Pravolamskaya <you@example.com>
2022-04-16 23:15:53 +0300

    tasks

--- 17_List_methods/06_unique_elems/main.py
+++ 17_List_methods/06_unique_elems/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+list_1 = []
+list_2 = []
+
+for i in range(3):
+    c = int(input('Enter  number '))
+    list_1.append(i)
+
+print('list 1: ', list_1)
+
+for a in range(7):
+    b = int(input('Enter number ',))
+    list_2.append(a)
+
+print('list 2: ', list_2 )
+
+list_1.extend(list_2)
+list_1 = list(set(list_1))
+print('cleared list: ', *list_1)
+
+
+
+
