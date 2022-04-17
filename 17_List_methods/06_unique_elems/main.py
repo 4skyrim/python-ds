930d9542d06a76ab3594fe9309f9104c4ed40380
Yaroslava Pravolamskaya <you@example.com>
2022-04-17 13:25:54 +0300

    tasks

--- 17_List_methods/06_unique_elems/main.py
+++ 17_List_methods/06_unique_elems/main.py
@@ -1,21 +1,27 @@
 list_1 = []
 list_2 = []
 
-for i in range(3):
+for _ in range(3):
     c = int(input('Enter  number '))
-    list_1.append(i)
+    list_1.append(c)
 
 print('list 1: ', list_1)
 
-for a in range(7):
+for _ in range(7):
     b = int(input('Enter number ',))
-    list_2.append(a)
+    list_2.append(b)
 
 print('list 2: ', list_2 )
 
 list_1.extend(list_2)
-list_1 = list(set(list_1))
+for elem in list_1:
+   count =  list_1.count(elem)
+   if count > 1:
+       for _ in range(count - 1):
+           list_1.remove(elem)
+
 print('cleared list: ', *list_1)
+
 
 
 
