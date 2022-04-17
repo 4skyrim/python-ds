f344f075cd5c80cc78bbafdcf455051477b7404a
Yaroslava Pravolamskaya <you@example.com>
2022-04-17 13:25:54 +0300

    tasks

--- 17_List_methods/02_line/main.py
+++ 17_List_methods/02_line/main.py
@@ -1,9 +1,4 @@
-list_1 = []
-list_2 = []
-for i_list in range(160, 170, 2):
-    list_1.append(i_list)
-for j_list in range(162,180, 3):
-    list_2.append(j_list)
-list_2.extend(list_1)
+list_1 = list(range(160, 177, 2))
+list_2 = list(range(162, 181, 3))
 list_2.sort()
-print(' sorted list: ', list_2)
+print(' sorted list: ', *list_2)
